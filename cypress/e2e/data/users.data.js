22059579af98218fa5adf9be9b863fe57460b61b
MariaMckenzie <68311077+MariaMckenzie@users.noreply.github.com>
2022-12-05 12:06:01 -0500

    Create users.data.js

---
+++ cypress/e2e/data/users.data.js
@@ -0,0 +1,22 @@
+module.exports = {
+    standardUser: {
+        username: 'standard_user',
+        password: 'secret_sauce',
+        errorMsg: ''
+    },
+    lockedOutUser: {
+        username: 'locked_out_user',
+        password: 'secret_sauce',
+        errorMsg: 'Sorry, this user has been locked out.'
+    },
+    problemUser: {
+        username: 'problem_user',
+        password: 'secret_sauce',
+        errorMsg: ''
+    },
+    performanceGlitchUser: {
+        username: 'performance_glitch_user',
+        password: 'secret_sauce',
+        errorMsg: ''
+    }
+}
